187824bae78b2ee37a15ec87c385fd2161521ae3
Akkarapong Garpaya <112610820+Akkarapo@users.noreply.github.com>
2024-08-08 14:16:36 +0700

    Quiz2 test2

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,10 +1,10 @@
 "use client";
 export default function Comment({
-  userImagePath,
-  username,
-  commentText,
-  likeNum,
-  replies,
+  //userImagePath,
+  //username,
+  //commentText,
+  //likeNum,
+  //replies,
 }) {
   return (
     <div>
